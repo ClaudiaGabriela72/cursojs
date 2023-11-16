c31554dba14fe614852d47cbd533d6178335e91f
ClaudiaGabriela72 <71900311+ClaudiaGabriela72@users.noreply.github.com>
2023-11-16 16:51:30 -0400

    ex010

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,2 +1,6 @@
 var vel = 65
 console.log(`A velocidade do altomovel é ${vel} km`)
+if (vel > 70){
+    console.log(`A velocidade foi ultrapassada, voce foi multado`)
+}
+console.log('dirija com cuidado')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'pais'
+if (pais == 'Brasil'){
+    console.log('é brasileira')
+} else{
+    console.log('é estrangeira')
+}
